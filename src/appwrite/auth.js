8a3688f4b755ac53dfd8eb248ee850fa159a3d2c
mahmedraza1 <developer.mahmedraza@gmail.com>
2025-08-18 21:38:44 +0500

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import conf from "../conf.js";
+import { Client, Account, ID } from "appwrite";
+
+class AppwriteAuth {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteEndpoint)
+      .setProject(conf.appwriteProjectID);
+    this.account = new Account(this.client);
+  }
+  async createAccount(email, password, name) {
+    try {
+      const userAcccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAcccount) {
+        return await this.login({ email, password });
+      }
+    } catch (error) {
+      console.error("Error creating account:", error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      await this.account.get();
+    } catch {
+      console.log("Error getting Account info", error);
+    }
+  }
+
+  async logout(){
+    try{
+        await this.account.deleteSession("current")
+    } catch{
+      console.log("Error getting logging out", error);
+    }
+  }
+
+}
+
+const authService = new AppwriteAuth();
+export default authService;
---
+++ src/appwrite/config.js
@@ -0,0 +1,134 @@
+import conf from "../.conf/conf";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+class AppwritePost {
+  client = new Client();
+  database;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteEndpoint)
+      .setProject(conf.appwriteProjectID);
+    this.database = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async creatPost({ title, content, featuredImage, status, userID, slug }) {
+    try {
+      return await this.database.createDocument(
+        conf.appwriteDatabaseID,
+        conf.appwriteCollectionID,
+        ID.unique(),
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userID,
+          slug,
+        }
+      );
+    } catch (error) {
+      console.log("Error Creating the Post", error);
+    }
+  }
+
+  async updatePost({ title, content, featuredImage, status, slug }, id) {
+    try {
+      return await this.database.updateDocument(
+        conf.appwriteDatabaseID,
+        conf.appwriteCollectionID,
+        id,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          slug,
+        }
+      );
+    } catch (error) {
+      console.log("Failed to Update the Document", error);
+    }
+  }
+
+  async getPost(id) {
+    try {
+      return await this.database.getDocument(
+        conf.appwriteDatabaseID,
+        conf.appwriteCollectionID,
+        id
+      );
+    } catch (error) {
+      console.log("Failed to Get the Post", error);
+    }
+  }
+
+  async deletePost(id) {
+    try {
+      return await this.database.getDocument(
+        conf.appwriteDatabaseID,
+        conf.appwriteCollectionID,
+        id
+      );
+    } catch (error) {
+      console.log("Failed to Delete Post", error);
+    }
+  }
+
+  async getPosts() {
+    try {
+      return await this.database.listDocuments(
+        conf.appwriteDatabaseID,
+        conf.appwriteCollectionID,
+        [Query.equal("status", "active")]
+      );
+    } catch (error) {
+      console.log("Failed to Get Posts", error);
+    }
+  }
+
+  // File Storage Service
+  async uploadFile(file){
+    try{
+      return await this.bucket.createFile(
+        conf.appwriteBucketID,
+        ID.unique(),
+        file
+      );
+    } catch(error){
+      console.log("Failed to Upload File", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileID){
+    try{
+      return await this.bucket.deleteFile(
+        conf.appwriteBucketID,
+        fileID
+      );
+    } catch(error){
+      console.log("Failed to Delete File", error);
+      return false;
+    }
+  }
+
+  async getFilePreview(fileID){
+    try{
+      return await this.bucket.getFilePreview(
+        conf.appwriteBucketID,
+        fileID
+      );
+    } catch(error){
+      console.log("Failed to Get File Preview", error);
+      return null;
+    }
+  }
+
+}
+
+const PostService = new AppwritePost();
+
+export default PostService;
