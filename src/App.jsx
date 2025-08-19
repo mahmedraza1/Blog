0b62787f0ff7742e0a4d6adbe91eb445ca6a5b3e
mahmedraza1 <developer.mahmedraza@gmail.com>
2025-08-19 09:15:42 +0500

    work on front end started

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authslice";
+import authService from "./appwrite/auth";
+import {Header, Footer} from "./Components/index";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return !loading ? (
+    <div className="w-screen h-screen bg-gray-900 text-white">
+      <Header />
+      <main>
+        <h1 className="text-2xl">Welcome to Mark's Blog</h1>
+       {/* TODO: <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+  ) : (
+    <div className="bg-gray-900 text-white w-screen h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Loading...</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Mark's Blog</h1>
+    </div>
+  )
+}
+
+export default Header
